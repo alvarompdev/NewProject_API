b0217adb506682147e24f923db68ce62c81cff74
alvarompdev <alvaromp.dev@gmail.com>
2025-06-23 00:38:10 +0200

    init commit with Spring Boot structure and PostgreSQL database

---
+++ backend/NewProject-API/src/test/java/alvarompdev/newprojectapi/NewProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package alvarompdev.newprojectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewProjectApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
